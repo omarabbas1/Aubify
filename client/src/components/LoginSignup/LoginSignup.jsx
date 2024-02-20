9ac3a2e7a575ad675f284a46755c4b4c8808d294
omarabbas1 <ofa15@mail.aub.edu>
2024-02-20 20:59:30 +0200

    updated directory

---
+++ client/src/components/LoginSignup/LoginSignup.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './LoginSignup.css'
+
+import user_icon from '../Assets/person.png'
+import email_icon from '../Assets/email.png'
+import password_icon from '../Assets/password.png'
+
+const LoginSignup = () => {
+  return (
+    <div>
+        
+    </div>
+  )
+}
+
+export default LoginSignup
