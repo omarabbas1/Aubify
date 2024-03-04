bbccf4447278d890ce59608cd9d9994e93df0a1a
JUSTH0P3 <mohamadyamout2004@gmail.com>
2024-03-04 10:04:19 +0200

    added several files and updated the homepage and posts jsx files

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -1,21 +1,28 @@
-import React from 'react';
 import Navbar from '../NavBar/NavBar';
+import Posts from '../Posts/Posts';
+import './HomePage.css';
 
 const HomePage = () => {
-  return (
-   /* const handleSignOut = () => {
+  const handleSubmitPost = (newPostContent) => {
+    // Here, you can perform any necessary operations with the new post content
+    console.log('New post content:', newPostContent);
+    // For now, let's just log the new post content
+  };
+
+  /* const handleSignOut = () => {
         / Handle sign out logic here
       };
       */
-    
 
+  return (
     <div className="home-page">
       <Navbar 
         websiteName="Aubify" 
         userName="User Name" 
        /* onSignOut={handleSignOut}     To handle the user's signout after creating a token.*/    
       />
-      <h1>Suiii</h1>
+       {/* Render the Posts component and pass the onSubmit function */}
+       <Posts handleSubmitPost={handleSubmitPost} />
     </div>
   );
 };
